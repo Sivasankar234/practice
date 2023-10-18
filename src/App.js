7ab185fabd8a11841157676461f0a9292bb97e4f
siva-sankar-007 <siva.sankar@thecloudside.com>
2023-10-18 15:19:28 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Chart } from "react-google-charts";
+import Gchart from './tooltip'
 function App() {
+  const data = [
+    ["Year", "Sales", "Expenses", "Profit"],
+    ["2014", 1000, 400, 200],
+    ["2015", 1170, 460, 250],
+    ["2016", 660, 1120, 300],
+    ["2017", 1030, 540, 350],
+  ];
+
+  const options = {
+    chart: {
+      title: "Company Performance",
+      subtitle: "Sales, Expenses, and Profit: 2014-2017",
+    },
+    tooltip: {
+      isHtml: true,
+      trigger: 'selection',
+      custom: (tooltip) => {
+        const dataIndex = tooltip.dataTable.getSelection()[0].row; // Get the selected row index
+        const year = data[dataIndex + 1][0]; // Get the year from your data array
+        const sales = data[dataIndex + 1][1]; // Get the sales value
+        const expenses = data[dataIndex + 1][2]; // Get the expenses value
+        const profit = data[dataIndex + 1][3]; // Get the profit value
+
+        return `
+          <div style="background-color: #f9f9f9; padding: 10px;">
+            <strong >Year:</strong> ${year}<br>
+            <strong>Sales:</strong> ${sales}<br>
+            <strong>Expenses:</strong> ${expenses}<br>
+            <strong>Profit:</strong> ${profit}
+          </div>
+        `;
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+        chartType="Bar"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+     
+      />
+     <Gchart/>
     </div>
   );
 }
---
+++ src/tooltip.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+
+function MyChart() {
+  useEffect(() => {
+    // Load the Google Charts library and set a callback function
+    window.google.charts.load('current', { packages: ['corechart'] });
+    window.google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      const data = new window.google.visualization.DataTable();
+      data.addColumn('string', 'Chart thing');
+      data.addColumn('number', 'Chart amount');
+      data.addRows([
+        ['Lorem ipsum', 60],
+        ['Dolor sit', 22],
+        ['Sit amet', 18],
+      ]);
+
+      const options = {
+        title: 'My Chart',
+        is3D: true, // You can customize the options as needed
+        tooltip: {
+            textStyle: {
+              className: 'custom-tooltip', // Apply your custom CSS class
+            },
+          },
+      };
+
+      const chart = new window.google.visualization.PieChart(document.getElementById('chart1'));
+      chart.draw(data, options);
+    }
+  }, []);
+
+  return <div id="chart1" style={{ width: '100%', height: '400px' }}></div>;
+}
+
+export default MyChart;
